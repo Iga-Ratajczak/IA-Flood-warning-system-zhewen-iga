d800b0c6fad88221eaa81d5547d193ecb48882fd
Iga Ratajczak <iga.ratajczak1502@gmail.com>
2022-02-05 18:04:25 +0000

    no tests, sorting issue

---
+++ Task1E.py
@@ -0,0 +1,20 @@
+from turtle import st
+from floodsystem.stationdata import build_station_list
+
+from floodsystem.geo import rivers_by_station_number
+
+def run():
+    """Requirements for Task 1D"""
+    
+
+    # Build list of stations
+    stations = build_station_list()
+    x=rivers_by_station_number(stations,9)
+    
+    print(x)
+
+
+if __name__ == "__main__":
+    print("*** Task 1D: CUED Part IA Flood Warning System ***")
+    
+    run()
