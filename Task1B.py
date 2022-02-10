2234a1e637b99d3335feda9a29145e9a3ec5c0f7
Iga Ratajczak <iga.ratajczak1502@gmail.com>
2022-02-10 11:52:11 +0000

    Complete task 1B

--- Task1B.py
+++ Task1B.py
@@ -1,14 +1,22 @@
 
 from floodsystem.stationdata import build_station_list
 from floodsystem.geo import stations_by_distance
+from haversine import haversine, Unit
 
 def run():
-    """Requirements for Task 1A"""
+    """Requirements for Task 1B"""
+    
 
     # Build list of stations
     stations = build_station_list()
-    x=0
-    print(stations_by_distance(stations,x))
+    cambridge = (52.2053, 0.1218) # (lat, lon)
+    temp=stations_by_distance(stations, cambridge)
+    print("Ten closest")
+    print (temp[:10])
+    print("ten furthest")
+    print (temp[-10:])
+
 if __name__ == "__main__":
     print("*** Task 1B: CUED Part IA Flood Warning System ***")
+    
     run()
