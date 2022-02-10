89dc1ebe970a02ba18023f83c1904fbf8308b733
Iga Ratajczak <iga.ratajczak1502@gmail.com>
2022-02-10 11:32:35 +0000

    Complete task D and E

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -5,7 +5,7 @@
 geographical data.
 
 """
-
+from math import dist
 from os import stat
 from .utils import sorted_by_key  # noqa
 from .station import MonitoringStation
@@ -18,6 +18,7 @@ from haversine import haversine, Unit
 
 #task 1B
 def stations_by_distance(stations, p):
+    '''function sorts stations by distance from the chosen location (need to be input as coordinate)'''
     print(type(p))
     
     #test if p is called,
@@ -26,17 +27,14 @@ def stations_by_distance(stations, p):
     list_with_distance=[]
 
     #test if stations are called
-    #test if sorted
+    
     for station in stations:
-        #temporary_tuple=()
         distance=haversine(station.coord, p)
         temporary_tuple=(station.name, station.town, distance)
         list_with_distance.append(temporary_tuple)
     list_with_distance = sorted_by_key(list_with_distance,2)
     
     return list_with_distance
-        #lyon = (45.7597, 4.8422) # (lat, lon)
-        #print(haversine(station.coord,p))
 
 #task 1C
 def stations_within_radius(stations, centre, r):
@@ -59,7 +57,8 @@ def rivers_with_station(stations):
 
 #part2
 def stations_by_river(stations):
-    #check stations called correctly
+    '''combines stations that are connected to the same river as a list'''
+    #test stations called correctly
     station_by_river = {}
     for station in stations:
         river=station.river
@@ -70,22 +69,49 @@ def stations_by_river(stations):
             station_by_river[station.river]=[station.name]           
     return station_by_river
 
+
 #task 1E
+
 def rivers_by_station_number(stations, N):
+    '''sorts rivers by the number of stations that are connected to it, in descending order'''
     river_list=[]
     dict_of_stations_river=stations_by_river(stations)
-  
+    #test stations called correctly
+    river_list_no_rep={}
+
     for station in stations:
         river=station.river
         if  river in dict_of_stations_river:
             tupletemp=()
-            #print(len(dict_of_stations_river[river]))
             tupletemp=(river, len(dict_of_stations_river[river]))
+            if river in river_list_no_rep.keys():
+                pass
+            else:
+                river_list_no_rep[river]=[0]
+                river_list.append(tupletemp)
+               
 
-        river_list.append(tupletemp)
+    #it sorts tuples in growing order by number of stations            
     river_list = sorted_by_key(river_list,1)
     #print(river_list)
+
     biggest=[]
+    i=0
     for i in range (N):
-        biggest.append(river_list[i])
+        if len(river_list)-i-1>-1:
+            biggest.append(river_list[len(river_list)-i-1])
+
+    #when there are more rivers with the same number of stations as the N th entry, include these rivers in the list. 
+    last_entry=river_list[len(river_list)-i-1]
+    number=len(river_list)-i-2
+    if number<0:
+        pass
+    else:
+        next_potential_entry=river_list[len(river_list)-i-2]
+        while last_entry[1]==next_potential_entry[1]:
+                biggest.append(next_potential_entry)
+                last_entry=next_potential_entry
+                next_potential_entry=river_list[len(river_list)-i-3]
+                i+=1
+
     return biggest
