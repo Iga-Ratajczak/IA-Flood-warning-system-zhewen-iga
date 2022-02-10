8b28658085f72310676d4e9c25d07e9c3bdb26d3
Iga Ratajczak <iga.ratajczak1502@gmail.com>
2022-02-10 11:40:08 +0000

    tests for task 1D

---
+++ test_stations_by_river.py
@@ -0,0 +1,16 @@
+from floodsystem.station import MonitoringStation
+from floodsystem.geo import stations_by_river
+
+def test_stations_by_river():
+    # Create a station
+    s_id = "test-s-id"
+    m_id = "test-m-id"
+    label = "some station"
+    coord = (-2.0, 4.0)
+    trange = (-2.3, 3.4445)
+    river = "River X"
+    town = "My Town"
+    s = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
+    x=stations_by_river(s)
+
+    assert x[river]==label
