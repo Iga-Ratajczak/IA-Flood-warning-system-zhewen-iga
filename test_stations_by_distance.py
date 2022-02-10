893e9dd1d96ffdb955e674d6932948ff352fc216
Iga Ratajczak <iga.ratajczak1502@gmail.com>
2022-02-10 11:40:19 +0000

    tests for task 1E

---
+++ test_stations_by_distance.py
@@ -0,0 +1,19 @@
+
+from floodsystem.station import MonitoringStation
+from floodsystem.geo import stations_by_distance
+from haversine import haversine, Unit
+
+def test_stations_by_distance():
+    # Create a station
+    s_id = "test-s-id"
+    m_id = "test-m-id"
+    label = "some station"
+    coord = (-2.0, 4.0)
+    trange = (-2.3, 3.4445)
+    river = "River X"
+    town = "My Town"
+    s = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
+
+    cambridge = (52.2053, 0.1218) # (lat, lon)
+    temp=stations_by_distance(s, cambridge)
+    assert round(temp,4)==6038
