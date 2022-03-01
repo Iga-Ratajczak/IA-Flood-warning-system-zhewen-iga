9184ffaf9f41e18e4f7f085f340e26dbf2cf4041
Iga Ratajczak <iga.ratajczak1502@gmail.com>
2022-03-01 13:13:40 +0000

    hi

--- Task2G.py
+++ Task2G.py
@@ -1,3 +1,4 @@
+from binascii import Incomplete
 from floodsystem.datafetcher import fetch_measure_levels
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.analysis import flood_risk
@@ -7,34 +8,43 @@ import datetime
 def run():
     stations = build_station_list()
     update_water_levels(stations)
+    dt = 1
+    dates, levels = [], []
     extremely_severe = []
     severe=[]
     high=[]
     moderate=[]
     low=[]
+    incomplete = []
     count = 0
     for station in stations:
         count += 1
-        dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=1))
-        risk = flood_risk(station, dates, levels, 4)
 
-        if risk == "Extremely Severe":
-            extremely_severe.append(station.name)
-        if risk == "Severe":
-            severe.append(station.name)
-        elif risk == "High":
-            high.append(station.name)
-        elif risk == "Moderate":
-            moderate.append(station.name)
-        elif risk == "Low":
-            low.append(station.name)
+        try:
+            dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
+            risk = flood_risk(station, dates, levels, 4)
+            if risk == "Extremely Severe":
+                    extremely_severe.append(station.name)
+            if risk == "Severe":
+                    severe.append(station.name)
+            elif risk == "High":
+                    high.append(station.name)
+            elif risk == "Moderate":
+                    moderate.append(station.name)
+            elif risk == "Low":
+                    low.append(station.name)
+        except:
+            incomplete.append(station.name)
+
+
         if count>200:
-            break
+                break
 
-    print(extremely_severe)
-    print(severe)
-    print(high)
-    print(moderate)
-    print(low)
+    print('stations with extremely severe risk:{}'.format(extremely_severe))
+    print('stations with severe risk:{}'.format(severe))
+    print('stations with high risk:{}'.format(high))
+    print('stations with moderate risk:{}'.format(moderate))
+    print('stations with low risk:{}'.format(low))
+    print('stations with incomplete data:{}'.format(incomplete))
 
 run()
--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -69,27 +69,26 @@ def polyfit(dates, levels, p):
 def flood_risk(station, dates, levels, p):
     risk = 0
     poly, offset = polyfit(dates, levels, p)
-    if poly != False:
-        x = mdates.date2num(dates)
-        x1 = np.linspace(x[-20], x[-1], 100)
-        grad = (poly(x1[49]-offset)-poly(x1[0]-offset))
-        if grad > 1.2:
-            risk += 2
-        elif grad > 0.6:
-            risk += 1
-        if station.relative_water_level > 2:
+    x = mdates.date2num(dates)
+    x1 = np.linspace(x[0], x[-1], 100)
+    grad = (poly(x1[49])-offset)-(poly(x1[0])-offset)
+    if grad > 1.2:
             risk += 3
-        elif station.relative_water_level > 1.5:
+    elif grad > 0.6:
             risk += 2
-        elif station.relative_water_level > 1:
-            risk += 1
-        if risk >= 5:
+    if station.relative_water_level() > 2:
+            risk += 4
+    elif station.relative_water_level() > 1.5:
+            risk += 3
+    elif station.relative_water_level() > 1:
+            risk += 2
+    if risk >= 5:
             return "Extremely Severe"
-        elif risk == 4:
+    elif risk == 4:
             return "Severe"
-        elif risk == 3:
+    elif risk == 3:
             return "High"
-        elif risk == 2:
+    elif risk == 2:
             return "Moderate"
-        else:
+    else:
             return "Low"
